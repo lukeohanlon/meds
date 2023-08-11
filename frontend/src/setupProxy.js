09ca182030b46500afb80bbf3bd9efc8e2cd7cd4
luke0han <lukeohanlon@outlook.com>
2023-08-11 18:15:08 +0100

    link

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://16.170.206.226:3000', 
+      target: 'http://16.171.145.223:3000', 
       changeOrigin: true,
     })
   );
